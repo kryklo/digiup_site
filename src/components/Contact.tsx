6a2d4a29f7a4374a968b985d32cb47761a032886
kryklo <145576676+kryklo@users.noreply.github.com>
2025-09-21 21:53:51 +0200

    Zaktualizuj tekst o polityce prywatności

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -306,7 +306,7 @@ const Contact = () => {
                   </p>
                 </div>
                 <div className="text-xs font-body text-gray-500 text-center">
-                  <p>Wysyłając formularz, wyrażasz zgodę na przetwarzanie danych osobowych w celu udzielenia odpowiedzi na zapytanie, zgodnie z naszą polityką prywatności. Dane będą przetwarzane przez DigiUp IT Consulting & Solutions Krystian Kłopocki.</p>
+                  <p>Wysyłając formularz, wyrażasz zgodę na przetwarzanie danych osobowych w celu udzielenia odpowiedzi na zapytanie, zgodnie z polityką prywatności. Dane będą przetwarzane przez DigiUp IT Consulting & Solutions Krystian Kłopocki.</p>
                 </div>
               </form>
             </div>
